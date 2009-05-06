d5b57be21911176feec13af20d64380a418247f6
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-05-06 03:48:27 +0000

    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@8 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/pool/server/s_vehicle_pool.lua
+++ src_trunk/resources/pool/server/s_vehicle_pool.lua
@@ -30,34 +30,3 @@ function getVehicleFromID(id)
 		return false
 	end
 end
-
-function showall()
-	outputDebugString("VEHICLES: " .. #pool)
-end
-addCommandHandler("showall", showall)
-
-function getall()
-	local players = #exports.pool:getAllPlayers()
-	local vehicles = #exports.pool:getAllVehicles()
-	local blips = #exports.pool:getAllBlips()
-	local teams = #exports.pool:getAllTeams()
-	local colshapes = #exports.pool:getAllColshapes() + #exports.pool:getAllColshapes()
-	local objects = #exports.pool:getAllObjects()
-	local pickups = #exports.pool:getAllPickups()
-	local markers = #exports.pool:getAllMarkers()
-	local peds = #exports.pool:getAllPeds()
-	--local total = players + vehicles + blips + teams + colshapes + objects + pickups + markers + peds
-	--outputDebugString("TOTAL: " .. total)
-	
-	outputDebugString("Vehicles: " .. vehicles)
-	outputDebugString("Players: " .. players)
-	outputDebugString("Markers: " .. markers)
-	outputDebugString("Pickups: " .. pickups)
-	outputDebugString("Objects: " .. objects)
-	outputDebugString("Blips: " .. blips)
-	outputDebugString("Colshapes: " .. colshapes)
-	outputDebugString("Teams: " .. teams)
-	outputDebugString("Peds: " .. peds)
-	
-end
-addCommandHandler("getall", getall)

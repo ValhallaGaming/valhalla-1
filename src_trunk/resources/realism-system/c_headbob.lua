7e3c29b3a3241746cdb80698334d731d5d60c909
Mr.Hankey <Mr.Hankey@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-05-22 20:11:47 +0000

    fixed 246 and made the local player headbob work like before
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@72 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_headbob.lua
+++ src_trunk/resources/realism-system/c_headbob.lua
@@ -3,11 +3,18 @@ function bobHead()
 	
 	if (logged==1) then
 		for key, value in ipairs(getElementsByType("player")) do
-			local rot = getPedCameraRotation(value)
-			local x, y, z = getElementPosition(value)
-			local vx = x + math.sin(math.rad(rot)) * 10
-			local vy = y + math.cos(math.rad(rot)) * 10
-			setPedLookAt(value, vx, vy, 10, 3000)
+            if value == getLocalPlayer() then
+                local scrWidth, scrHeight = guiGetScreenSize()
+        		local sx = scrWidth/2
+        		local sy = scrHeight/2
+        		local x, y, z = getWorldFromScreenPosition(sx, sy, 10)
+        		setPedLookAt(value, x, y, z, 3000)
+            else
+                local rot = getPedCameraRotation(value)
+    			local x, y, z = getElementPosition(value)
+    			local vx = x + math.sin(math.rad(rot)) * 10
+    			local vy = y + math.cos(math.rad(rot)) * 10
+    			setPedLookAt(value, vx, vy, z, 3000)
 		end
 	end
 end

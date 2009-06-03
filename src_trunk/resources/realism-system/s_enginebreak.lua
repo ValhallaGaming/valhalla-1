49ef1b249781d84a70f87b09b65f39914feb2c04
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-03 03:32:52 +0000

    Fix for 327
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@119 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/s_enginebreak.lua
+++ src_trunk/resources/realism-system/s_enginebreak.lua
@@ -2,13 +2,23 @@ function engineBreak()
 	local health = getElementHealth(source)
 	local driver = getVehicleController(source)
 	
-	if (driver) and (health<=400) then
-		local rand = math.random(1, 10)
+	if (driver) then
+		if (health<=400)
+			local rand = math.random(1, 5)
 
-		if (rand==1) then -- 10% chance
-			setVehicleEngineState(source, false)
-			setElementData(source, "engine", 0)
-			exports.global:sendLocalDoAction(driver, "The engine breaks down.")
+			if (rand==1) then -- 20% chance
+				setVehicleEngineState(source, false)
+				setElementData(source, "engine", 0)
+				exports.global:sendLocalDoAction(driver, "The engine stalls due to damage.")
+			end
+		elseif (health<=300) then
+			local rand = math.random(1, 2)
+
+			if (rand==1) then -- 50% chance
+				setVehicleEngineState(source, false)
+				setElementData(source, "engine", 0)
+				exports.global:sendLocalDoAction(driver, "The engine breaks down.")
+			end
 		end
 	end
 end

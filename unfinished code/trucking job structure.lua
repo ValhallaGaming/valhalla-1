c302907a0e2683c8622867a600148718ca4a0769
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-11 18:23:46 +0000

    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@152 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- unfinished code/trucking job structure.lua
+++ unfinished code/trucking job structure.lua
@@ -1,4 +1,4 @@
-RSHaulTrucks = {[]=true, []=true, []=true, []=true, []=true} -- need to /makecivveh. The trucks and trailers are permanently spawned.
+RSHaulTruck = {[]=true, []=true, []=true, []=true, []=true} -- need to /makecivveh. The trucks and trailers are permanently spawned.
 
 -- Trucking Delivery Points
 local point1 = ( , , )
@@ -16,9 +16,10 @@ local point10 = ( , , )
 local lastDeliveryPoint = 0
 local revenue = 0
 
+-- Phase 1: Initiate the job.
 function truckJob (commandName, thePlayer)
 
-	if not (isPedInVehicle(thePlayer)) and (getPedOccupiedVehicle(RSHaulTrucks) then -- Is the player in a RS Haul vehicle
+	if not (isPedInVehicle(thePlayer)) and (getPedOccupiedVehicle(RSHaulTruck) then -- Is the player in a RS Haul vehicle
 		outputChatBox("You must be in an RS Haul truck to begin deliveries.", thePlayer, 255, 0, 0)
 	else
 		local jobID = -- get the players job ID.
@@ -32,44 +33,44 @@ function truckJob (commandName, thePlayer)
 end
 addCommandHandler ("truckjob", truckJob)
 
-function createDeliveryPoint(thePlayer)
+-- Phase 2: Select a delivery point.
+function createDeliveryPointHandler(thePlayer)
 
 	local deliveryPoint = math.random(1,10) -- select a random point
 	
-	if (deliveryPoint) == (oldDeliveryPoint) then
-		local deliveryPoint = math.random(1,10) -- select another random point
+	if (deliveryPoint) == (lastDeliveryPoint) then
+		triggerServerEvent ("createDeliveryPoint")
 	else
-		triggerServerEvent ("startTrucking")
-end
-
-function startTrucking (thePlayer)
-
-	local lastDeliveryPoint = deliveryPoint
+		local lastDeliveryPoint = deliveryPoint
 		
-	local blip = createBlip() -- create radar blip at delivery point.
-	local marker = createMarker() -- create marker at delivery point.
-	attachElements(marker, blip)
-	exports.pool:allocateElement(blip)
-	exports.pool:allocateElement(marker)
+		local blip = createBlip() -- create radar blip at delivery point.
+		local marker = createMarker() -- create marker at delivery point.
+		local colsphere = createColSphere(284.39205932617, 1411.373046875, 10.4046459198, 4) -- Create colision sphere where marker is.
+		attachElements(marker, blip)
+		exports.pool:allocateElement(blip)
+		exports.pool:allocateElement(marker)
 
-	setElementVisibleTo(marker, getRootElement(), false)
-	setElementVisibleTo(marker, thePlayer, true)
-	setElementVisibleTo(blip, getRootElement(), false)
-	setElementVisibleTo(blip, thePlayer, true)
-	
-	setElementData(marker, "owner", getElementData(thePlayer, "gameaccountid"))
-	setElementData(marker, "type", "job")
-	setElementData(blip, "owner", getElementData(thePlayer, "gameaccountid"))
-	setElementData(blip, "type", "job")
-	
-	addEventHandler("onColShapeHit", colsphere, dropOff) -- when the player enters the marker trigger the next stage.
-
+		setElementVisibleTo(marker, getRootElement(), false)
+		setElementVisibleTo(marker, thePlayer, true)
+		setElementVisibleTo(blip, getRootElement(), false)
+		setElementVisibleTo(blip, thePlayer, true)
+		
+		setElementData(marker, "owner", getElementData(thePlayer, "gameaccountid"))
+		setElementData(marker, "type", "job")
+		setElementData(blip, "owner", getElementData(thePlayer, "gameaccountid"))
+		setElementData(blip, "type", "job")
+		
+		addEventHandler("onColShapeHit", colsphere, dropOff) -- when the player enters the marker trigger the next stage.
+		
+	end
 end
+addEvent ("createDeliveryPoint")
+addEventHandler ("createDeliveryPoint", getRootElement, createDeliveryPointHandler )
 
-
+-- Phase 3: Drop off point.
 function dropOff(thePlayer)
 	
-	-- Check the truck has a trailer attacthed.
+	-- Check the truck is a RS Haul trucj and a trailer is attacthed.
 	local vehicle = getPedOccupiedVehicle(thePlayer) 
 	local trailer = getVehicleTowedByVehicle(vehicle)
 	
@@ -77,22 +78,41 @@ function dropOff(thePlayer)
 		outputChatBox("Depot says: You lost the trailer!", thePlayer, 255, 255, 255) -- You forgot the trailer
 	else
 	
-		local revenue = revenue + 50
-		outputChatBox("Depot says: Your next delivery is now shown on your GPS.", thePlayer, 255, 255, 255)
-		outputChatBox("You can return to the truck depot to collect your earnings or do another delievery.", thePlayer, 255, 0, 0)
+		setElementData (thePlayer, "revenue", (getElementData(thePlayer, "revenue") + 50)
+		outputChatBox("Depot says: Better late than never. Your next delivery is now shown on your GPS.", thePlayer, 255, 255, 255)
+		outputChatBox("You can return to the truck depot in LV to collect your earnings or do another delivery.", thePlayer, 255, 0, 0)
 	
-		-- add a "finish" marker to the depot.
-			
+		-- add a "finish" marker at the depot.
+		local depotBlip = createBlip() -- create radar blip at LV depot.
+		local depotMarker = createMarker() -- create marker at LV depot.
+		local depotColsphere = createColSphere(284.39205932617, 1411.373046875, 10.4046459198, 4) -- Create colision sphere where marker is.
+		attachElements(depotMarker, depotBlip)
+		exports.pool:allocateElement(depotBlip)
+		exports.pool:allocateElement(depotMarker)	
+		
+		addEventHandler("onColShapeHit", depotColsphere, dropOff) -- when the player enters the marker trigger the next stage.
+		
 		triggerServerEvent("createDeliveryPoint") -- create a new delivery destination.
 		
 	end
 end
 
+-- Phase 4: Return to depot.
 function finishTruckJob (thePlayer) -- When the player enters the marker back at the truck depot.
-
-	-- Remove the player from the vehicle
-	-- Respawn the vehicle.
-	-- clear all blips and markers.
-	-- Pay the player the "revenue".
+	
+	local vehicle = getPedOccupiedVehicle(thePlayer)
+	local trailer = getVehicleTowedByVehicle(vehicle)
+	removePedFromVehicle(thePlayer) -- Remove the player from the vehicle
+	respawnVehicle(vehicle) -- Respawn the vehicle.
+	respawnVehicle(trailer)
+	destroyElement(blip) -- clear all blips and markers.
+	destroyElement(marker)
+	destroyElement(colsphere)
+	destroyElement(depotBlip) -- clear all blips and markers.
+	destroyElement(depotMarker)
+	destroyElement(depotColsphere)
+	
+	local earnings = getElementData(thePlayer, "revenue")
+	exports.global:givePlayerSafeMoney(thePlayer, tonumber(earnings)) -- Pay the player the "revenue".
 	
 end

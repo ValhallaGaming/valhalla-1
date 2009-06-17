0d3034e858b08e2ef41da83b2ddcbcf613f6764c
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-17 04:28:29 +0000

    New item global for clientside retrieval of an item name to save bandwidth & clock cycles
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@171 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/global/c_item_globals.lua
+++ src_trunk/resources/global/c_item_globals.lua
@@ -25,3 +25,67 @@ function cdoesPlayerHaveItem(thePlayer, itemID, itemValue)
 	end
 	return false
 end
+
+function cgetItemName(itemID)
+	if (itemID==1) then
+		return "Haggis"
+	elseif (itemID==2) then
+		return "Cellphone"
+	elseif (itemID==3) then
+		return "Car Key"
+	elseif (itemID==4) then
+		return "House Key"
+	elseif (itemID==5) then
+		return "Business Key"
+	elseif (itemID==6) then
+		return "Radio"
+	elseif (itemID==7) then
+		return "Phonebook"
+	elseif (itemID==8) then
+		return "Sandwich"
+	elseif (itemID==9) then
+		return "Softdrink"
+	elseif (itemID==10) then
+		return "Dice"
+	elseif (itemID==11) then
+		return "Taco"
+	elseif (itemID==12) then
+		return "Burger"
+	elseif (itemID==13) then
+		return "Donut"
+	elseif (itemID==14) then
+		return "Cookie"
+	elseif (itemID==15) then
+		return "Water"
+	elseif (itemID==16) then
+		return "Clothes"
+	elseif (itemID==17) then
+		return "Watch"
+	elseif (itemID==18) then
+		return "City Guide"
+	elseif (itemID==19) then
+		return "MP3 Player"
+	elseif (itemID==20) then
+		return "Standard Fighting for Dummies"
+	elseif (itemID==21) then
+		return "Boxing for Dummies"
+	elseif (itemID==22) then
+		return "Kung Fu for Dummies"
+	elseif (itemID==23) then
+		return "Knfee Head Fighting for Dummies"
+	elseif (itemID==24) then
+		return "Grab Kick Fighting for Dummies"
+	elseif (itemID==25) then
+		return "Elbow Fighting for Dummies"
+	elseif (itemID==26) then
+		return "Gas Mask"
+	elseif (itemID==27) then
+		return "Flashbang"
+	elseif (itemID==28) then
+		return "Glowstick"
+	elseif (itemID==29) then
+		return "Door Ram"
+	else
+		return false
+	end
+end

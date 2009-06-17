af1112abc0a06fda364670ef47b9ced0126715f9
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-17 04:53:54 +0000

    F1 resource for chamberlain to optomize Map changes
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@173 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/social-system/c_player_rightclick.lua
+++ src_trunk/resources/social-system/c_player_rightclick.lua
@@ -1,12 +1,12 @@
 wRightClick = nil
-bAddAsFriend, bCloseMenu = nil
+bAddAsFriend, bFrisk, bRestrain, bCloseMenu = nil
 sent = false
 ax, ay = nil
 player = nil
 
 function clickPlayer(button, state, absX, absY, wx, wy, wz, element)
 	if (element) then
-		if (getElementType(element)=="player") and (button=="right") and (sent==false) and (element~=getLocalPlayer()) then
+		if (getElementType(element)=="player") and (sent==false) and (element==getLocalPlayer()) then
 			if (wRightClick) then
 				hidePlayerMenu()
 			end
@@ -42,12 +42,55 @@ function showPlayerMenu(targetPlayer, friends, description)
 		addEventHandler("onClientGUIClick", bAddAsFriend, cremoveFriend, false)
 	end
 	
-	bAddAsFriend = guiCreateButton(0.05, 0.25, 0.87, 0.1, "Close Menu", true, wRightClick)
+	bAddAsFriend = guiCreateButton(0.05, 0.45, 0.87, 0.1, "Close Menu", true, wRightClick)
 	addEventHandler("onClientGUIClick", bAddAsFriend, hidePlayerMenu, false)
 	sent = false
+	
+	-- FRISK
+	bFrisk = guiCreateButton(0.05, 0.25, 0.87, 0.1, "Frisk", true, wRightClick)
+	addEventHandler("onClientGUIClick", bFrisk, cfriskPlayer, false)
 end
 addEvent("displayPlayerMenu", true)
 addEventHandler("displayPlayerMenu", getRootElement(), showPlayerMenu)
+
+--------------------
+--    FRISKING    --
+--------------------
+wFriskItems, bFriskTakeItem, bFriskClose, gFriskItems, FriskColName = nil
+function cfriskPlayer(button, state, x, y)
+	if not (wFriskItems) then
+		local width, height = 300, 200
+		local scrWidth, scrHeight = guiGetScreenSize()
+		
+		wFriskItems = guiCreateWindow(x, y, width, height, "Frisk: " .. getPlayerName(player), false)
+		guiWindowSetSizable(wFriskItems, false)
+		
+		gFriskItems = guiCreateGridList(0.05, 0.1, 0.9, 0.7, true, wFriskItems)
+		FriskColName = guiGridListAddColumn(gFriskItems, "Name", 0.9)
+		
+		local items = getElementData(player, "items")
+		
+		for i = 1, 10 do
+			local itemID = tonumber(gettok(items, i, string.byte(',')))
+			if (itemID~=nil) then
+				local itemName = exports.global:cgetItemName(itemID)
+				outputChatBox(tostring(itemName))
+				local row = guiGridListAddRow(gFriskItems)
+				guiGridListSetItemText(gFriskItems, row, FriskColName, tostring(itemName), false, false)
+			end
+		end
+		
+		bFriskTakeItem = guiCreateButton(0.05, 0.85, 0.45, 0.1, "Take Item", true, wFriskItems)
+		bFriskClose = guiCreateButton(0.5, 0.85, 0.45, 0.1, "Close", true, wFriskItems)
+		addEventHandler("onClientGUIClick", bFrisk, hidePlayerMenu, false)
+	else
+		guiBringToFront(wFriskItems, true)
+	end
+end
+--------------------
+--  END FRISKING  --
+--------------------
+
 
 function caddFriend()
 	triggerServerEvent("addFriend", getLocalPlayer(), player)

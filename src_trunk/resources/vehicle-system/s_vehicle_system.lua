dcb249f2d8e0f6715c480a9b3e45523c2b84dc51
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-25 22:05:36 +0000

    0000464: Car owner message.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@284 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/vehicle-system/s_vehicle_system.lua
+++ src_trunk/resources/vehicle-system/s_vehicle_system.lua
@@ -766,6 +766,23 @@ addEventHandler("onVehicleStartEnter", getRootElement(), checkBikeLock)
 
 function setRealInVehicle(thePlayer)
 	setElementData(thePlayer, "realinvehicle", 1)
+	
+	-- 0000464: Car owner message. 
+	local owner = getElementData(source, "owner")
+	local faction = getElementData(source, "faction")
+	local carName = getVehicleName(source)
+	
+	if (owner<0) then
+		outputChatBox("(( This " .. carName .. " is an admin vehicle. ))", thePlayer, 255, 195, 14)
+	elseif (faction==-1) and (owner>0) then
+		local query = mysql_query(handler, "SELECT charactername FROM characters WHERE id='" .. owner .. "' LIMIT 1")
+		
+		if (mysql_num_rows(query)>0) then
+			local ownerName = mysql_result(query, 1, 1)
+			outputChatBox("(( This " .. carName .. " belongs to " .. ownerName .. ". ))", thePlayer, 255, 195, 14)
+		end
+		mysql_free_result(query)
+	end
 end
 addEventHandler("onVehicleEnter", getRootElement(), setRealInVehicle)
 

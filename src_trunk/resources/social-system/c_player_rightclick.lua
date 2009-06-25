9510d5f7a18c00b2f301bc5ba5d915c090265b00
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-25 13:47:06 +0000

    Bug fix for right click
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@274 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/social-system/c_player_rightclick.lua
+++ src_trunk/resources/social-system/c_player_rightclick.lua
@@ -7,7 +7,7 @@ gotClick = false
 closing = false
 
 function clickPlayer(button, state, absX, absY, wx, wy, wz, element)
-	if (element) and (getElementType(element)=="player") and (button=="right") and (state=="down") and (sent==false) and (element==getLocalPlayer()) then
+	if (element) and (getElementType(element)=="player") and (button=="right") and (state=="down") and (sent==false) and (element~=getLocalPlayer()) then
 		if (wRightClick) then
 			hidePlayerMenu()
 		end

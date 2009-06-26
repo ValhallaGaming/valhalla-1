59702e0d1bc2ab518a7e80f7a1eb42532a66f6aa
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-26 01:56:47 +0000

    ES Commands
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@285 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/faction-system/s_duty_commands.lua
+++ src_trunk/resources/faction-system/s_duty_commands.lua
@@ -8,11 +8,160 @@ exports.pool:allocateElement(pdColShape2)
 setElementDimension(pdColShape2, 681)
 setElementInterior(pdColShape2, 6)
 
+esColShape = createColSphere(1581.2067871094, 1790.4083251953, 2083.3837890625, 5)
+exports.pool:allocateElement(esColShape)
+setElementInterior(esColShape, 4)
+
 --- DUTY TYPE
 -- 0 = NONE
 -- 1 = SWAT
 -- 2 = Police
 -- 3 = Cadet
+-- 4 = ES
+-- 5 = FIRE ES
+
+-- ES
+function lvesHeal(thePlayer, commandName, targetPartialNick, price)
+	if not (targetPartialNick) or not (price) then -- if missing target player arg.
+		outputChatBox("SYNTAX: /" .. commandName .. " [Player Partial Nick / ID] [Price of Heal]", thePlayer, 255, 194, 14)
+	else
+	    local targetPlayer = exports.global:findPlayerByPartialNick(targetPartialNick)
+		if not (targetPlayer) then -- is the player online?
+			outputChatBox("Player not found.", thePlayer, 255, 0, 0)
+	    else
+		    local logged = getElementData(thePlayer, "loggedin")
+	
+	        if (logged==1) then
+	            local theTeam = getPlayerTeam(thePlayer)
+		        local factionType = getElementData(theTeam, "type")
+				
+				if not (factionType==4) then
+				    outputChatBox("You have no basic medic skills, contact the ES.", thePlayer, 255, 0, 0)
+				else
+			        price = tonumber(price)
+				
+				    local targetPlayerName = getPlayerName(targetPlayer)
+		            local x, y, z = getElementPosition(thePlayer)
+		            local tx, ty, tz = getElementPosition(targetPlayer)
+		        
+		            if (getDistanceBetweenPoints3D(x, y, z, tx, ty, tz)>5) then -- Are they standing next to each other?
+				        outputChatBox("You are too far away to heal '"..getPlayerName(targetPlayerName).."'.", thePlayer, 255, 0, 0)
+	                else
+				        local money = getElementData(targetPlayer, "money")
+							
+					    if (price>money) then
+						    outputChatBox("The player cannot afford the heal.", thePlayer, 255, 0, 0)
+				        else
+							outputDebugString("HIT")
+							exports.global:takePlayerSafeMoney(targetPlayer, price)
+					        setElementHealth(targetPlayer, 100)
+					        outputChatBox("You have been healed by '" ..getPlayerName(thePlayer).. "'.", targetPlayer, 0, 255, 0)
+					    end
+					end
+				end
+			end
+		end
+	end
+end
+addCommandHandler("heal", lvesHeal, false, false)
+
+function lvesduty(thePlayer, commandName)	
+	local logged = getElementData(thePlayer, "loggedin")
+
+	if (logged==1) then
+		if (isElementWithinColShape(thePlayer, esColShape)) then
+		
+			local duty = tonumber(getElementData(thePlayer, "duty"))
+			local theTeam = getPlayerTeam(thePlayer)
+			local factionType = getElementData(theTeam, "type")
+			
+			if (factionType==4) then
+				if (duty==0) then
+					outputChatBox("You are now on Medic Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "takes their uniform from their locker.")
+					
+					setElementData(thePlayer, "casualskin", getPedSkin(thePlayer))
+					
+					takeAllWeapons(thePlayer)
+					setElementHealth(thePlayer, 100)
+					
+					giveWeapon(thePlayer, 41, 5000) -- Pepperspray
+					setPedSkin(thePlayer, 276)
+					
+					setElementData(thePlayer, "duty", 4)
+				elseif (duty==4) then -- ES
+					takeAllWeapons(thePlayer)
+					outputChatBox("You are now off Medic Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "puts their uniform into their locker.")
+					setElementData(thePlayer, "duty", 0)
+					
+					local casualskin = getElementData(thePlayer, "casualskin")
+					setPedSkin(thePlayer, casualskin)
+				elseif (duty==5) then -- FIRE ES
+					takeAllWeapons(thePlayer)
+					outputChatBox("You are now off Firefighter Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "puts their firefighter gear into their locker.")
+					setElementData(thePlayer, "duty", 0)
+					
+					local casualskin = getElementData(thePlayer, "casualskin")
+					setPedSkin(thePlayer, casualskin)
+				end
+			end
+		end
+	end
+end
+addCommandHandler("duty", lvesduty, false, false)
+
+-- ES FD
+function lvfdduty(thePlayer, commandName)	
+	local logged = getElementData(thePlayer, "loggedin")
+
+	if (logged==1) then
+		if (isElementWithinColShape(thePlayer, esColShape)) then
+		
+			local duty = tonumber(getElementData(thePlayer, "duty"))
+			local theTeam = getPlayerTeam(thePlayer)
+			local factionType = getElementData(theTeam, "type")
+			
+			if (factionType==4) then
+				if (duty==0) then
+					outputChatBox("You are now on Firefighter Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "takes their firefighter gear from their locker.")
+					
+					setElementData(thePlayer, "casualskin", getPedSkin(thePlayer))
+					
+					takeAllWeapons(thePlayer)
+					setElementHealth(thePlayer, 100)
+					
+					giveWeapon(thePlayer, 42, 5000) -- Fire Extinguisher
+					giveWeapon(thePlayer, 9, 1) -- Chainsaw
+					giveWeapon(thePlayer, 41, 5000) -- Pepperspray
+					setPedSkin(thePlayer, 278)
+					
+					setElementData(thePlayer, "duty", 5)
+				elseif (duty==4) then -- ES
+					takeAllWeapons(thePlayer)
+					outputChatBox("You are now off Medic Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "puts their Sgear into their locker.")
+					setElementData(thePlayer, "duty", 0)
+					
+					local casualskin = getElementData(thePlayer, "casualskin")
+					setPedSkin(thePlayer, casualskin)
+				elseif (duty==5) then -- FIRE ES
+					takeAllWeapons(thePlayer)
+					outputChatBox("You are now off Firefighter Duty.", thePlayer)
+					exports.global:sendLocalMeAction(thePlayer, "puts their firefighter gear into their locker.")
+					setElementData(thePlayer, "duty", 0)
+					
+					local casualskin = getElementData(thePlayer, "casualskin")
+					setPedSkin(thePlayer, casualskin)
+				end
+			end
+		end
+	end
+end
+addCommandHandler("firefighter", lvfdduty, false, false)
+
 function pdarmor(thePlayer, commandName)
 	local logged = getElementData(thePlayer, "loggedin")
 

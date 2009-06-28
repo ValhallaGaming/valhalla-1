7441f12bdef2a9a9d4d9103533d267866e723bed
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-06-28 05:35:29 +0000

    Bug fix for clientside animations
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@329 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/global/c_animation_globals.lua
+++ src_trunk/resources/global/c_animation_globals.lua
@@ -7,3 +7,34 @@ function syncAnimation(block, name, speed, blendSpeed, startTime, loop, updatePo
 end
 addEvent("syncAnimation", true)
 addEventHandler("syncAnimation", getRootElement(), syncAnimation)
+
+function applyAnimation(thePlayer, block, name, speed, blendSpeed, startTime, loop, updatePosition, forced)
+	if speed==nil then speed = 1.0 end
+	if blendSpeed==nil then blendSpeed = 1.0 end
+	if startTime==nil then startTime = 0.0 end
+	if loop==nil then loop = true end
+	if updatePosition==nil then updatePosition = true end
+	if forced==nil then forced = true end
+	
+	if (isElement(thePlayer) and getElementType(thePlayer)=="player") then
+		toggleAllControls(false, true, false)
+		setElementData(thePlayer, "forcedanimation", true, forced)
+		setElementData(thePlayer, "animation", true, true)
+		blendPedAnimation(thePlayer, block, name, speed, blendSpeed, startTime, loop, updatePosition, true)
+		return true
+	else
+		return false
+	end
+end
+
+function removeAnimation(thePlayer)
+	if (isElement(thePlayer) and getElementType(thePlayer)=="player") then
+		local setanim = setPedAnimation(thePlayer)
+		setElementData(thePlayer, "forcedanimation", true, false)
+		setElementData(thePlayer, "animation", true, false)
+		toggleAllControls(true, true, false)
+		return setanim
+	else
+		return false
+	end
+end

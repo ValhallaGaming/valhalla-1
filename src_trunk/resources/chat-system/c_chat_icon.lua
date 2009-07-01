9d4fc4499b62cb748a8b3070421be0660c9a15d1
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-01 04:13:13 +0000

    0000484: Reduce the range chat icons are viewable from.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@343 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/chat-system/c_chat_icon.lua
+++ src_trunk/resources/chat-system/c_chat_icon.lua
@@ -13,17 +13,18 @@ setElementData(getLocalPlayer(), "chatting", true, 0)
 function render()
 	local x, y, z = getElementPosition(getLocalPlayer())
 	for key, value in ipairs(getElementsByType("player")) do
-		if (value~=getLocalPlayer()) then
+		if (value==getLocalPlayer()) then
 			local chatting = getElementData(value, "chatting")
 			
 			if (chatting==1) then
-				local px, py, pz = getElementPosition(value)
+				--local px, py, pz = getElementPosition(value)
+				local px, py, pz = getPedBonePosition(value, 6)
 				
 				local dist = getDistanceBetweenPoints3D(x, y, z, px, py, pz)
 				if (dist < 300) then
 					local reconning = getElementData(value, "reconx")
 					if (isLineOfSightClear(x, y, z, px, py, pz, true, false, false, false ) and isElementOnScreen(value)) and not (reconning) then
-						local screenX, screenY = getScreenFromWorldPosition(px, py, pz+1.2)
+						local screenX, screenY = getScreenFromWorldPosition(px, py, pz+0.5)
 						if (screenX and screenY) then
 							local draw = dxDrawImage(screenX, screenY, 70, 70, "chat.png")
 						end

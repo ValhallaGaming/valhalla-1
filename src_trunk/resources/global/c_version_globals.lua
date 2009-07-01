48e1ab835782ed5db6f77f02c4d8496744fc0418
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-01 08:34:22 +0000

    Version increment
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@372 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/global/c_version_globals.lua
+++ src_trunk/resources/global/c_version_globals.lua
@@ -1,4 +1,4 @@
-cscriptVersion = "2.1 r367"
+cscriptVersion = "2.1 r372"
 function cgetScriptVersion()
 	return cscriptVersion
 end
--- src_trunk/resources/global/s_version_globals.lua
+++ src_trunk/resources/global/s_version_globals.lua
@@ -1,4 +1,4 @@
-scriptVersion = "2.1 r367"
+scriptVersion = "2.1 r372"
 function getScriptVersion()
 	return scriptVersion
 end

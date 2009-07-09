00ef95c3a487dcdbf917562111bfed38da45799d
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-09 22:23:55 +0000

    Added an achievement.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@429 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/job-system/s_fishing_job.lua
+++ src_trunk/resources/job-system/s_fishing_job.lua
@@ -16,7 +16,7 @@ function startFishing(thePlayer)
 			else
 				local x, y, z = getElementPosition(thePlayer)
 					
-				if (x < 3000) then -- the further out to sea you go the bigger the fish you will catch.
+				if (x < 3000) or (y > 4000) then -- the further out to sea you go the bigger the fish you will catch.
 					outputChatBox("You must be out at sea to fish.", thePlayer, 255, 0, 0)
 				else
 					if not(exports.global:doesPlayerHaveItem(thePlayer, 49)) then -- does the player have the fishing rod item?
@@ -33,18 +33,18 @@ function startFishing(thePlayer)
 								local catchTimer = setTimer (catchFish, biteTimer, 1, thePlayer) -- A fish will bite within 1 and 5 minutes.
 								exports.global:sendLocalMeAction(thePlayer,"casts a fishing line.")
 										
-								if not (colSphere) then -- If the /sellfish marker isnt already being shown...
-									blip = createBlip( 2248.8425292969, 576.97155761719, 7.7802104949951, 0, 2, 255, 0, 255, 255 )
-									marker = createMarker( 2248.8425292969, 576.97155761719, 7.7802104949951, "cylinder", 2, 255, 0, 255, 150 )
-									colsphere = createColSphere (2248.8425292969, 576.97155761719, 7.7802104949951, 3)
+								if not (colsphere) then -- If the /sellfish marker isnt already being shown...
+									blip = createBlip( 2243.7339, 578.905, 6.78, 0, 2, 255, 0, 255, 255 )
+									marker = createMarker( 2243.7339, 578.905, 6.78, "cylinder", 2, 255, 0, 255, 150 )
+									colsphere = createColSphere (2243.7339, 578.905, 6.78, 3)
 									
 									setElementVisibleTo(blip, getRootElement(), false)
 									setElementVisibleTo(blip, thePlayer, true)
 									setElementVisibleTo(marker, getRootElement(), false)
 									setElementVisibleTo(marker, thePlayer, true)
 
-									outputChatBox("#FF9933When you are done fishing you can sell your catch at the fish monger ((#FF66CCblip#FF9933 added to radar)).", thePlayer, 255, 104, 91, true)
-									outputChatBox("#FF9933((To sell your catch enter /sellfish in the #FF66CCmarker#FF9933.))", thePlayer, 255, 104, 91, true)
+									outputChatBox("#FF9933When you are done fishing you can sell your catch at the fish market ((#FF66CCblip#FF9933 added to radar)).", thePlayer, 255, 104, 91, true)
+									outputChatBox("#FF9933((/totalcatch to see how much you have caught. To sell your catch enter /sellfish in the #FF66CCmarker#FF9933.))", thePlayer, 255, 104, 91, true)
 								end
 							end
 						end
@@ -59,7 +59,7 @@ addCommandHandler("fish", startFishing, false, false)
 function catchFish(thePlayer)
 	local lineSnap = math.random(1,10)
 	if (lineSnap > 9) then
-		exports.global:takePlayerItem(thePlayer, 49) -- fishing rod
+		exports.global:takePlayerItem(thePlayer, 49, 1) -- fishing rod
 		exports.global:sendLocalMeAction(thePlayer,"snaps their fishing line.")
 		outputChatBox("Your fishing rod has broken. You need to buy a new one to continue fishing.", thePlayer, 255, 0, 0)
 	else
@@ -79,6 +79,10 @@ function catchFish(thePlayer)
 		exports.global:sendLocalMeAction(thePlayer,"catches a fish weighing ".. fishSize .."lbs.")
 		totalCatch = totalCatch + fishSize
 		outputChatBox("You have "..totalCatch.."lbs of fish caught so far.", thePlayer, 255, 194, 14)
+		
+		if (fishSize >= 100) then
+			exports.global:givePlayerAchievement(thePlayer, 35)
+		end
 	end
 end
 function currentCatch(thePlayer)
@@ -88,19 +92,23 @@ addCommandHandler("totalcatch", currentCatch, false, false)
 
 function unloadCatch(thePlayer)
 	if (isElementWithinColShape(thePlayer, colsphere)) then
-		local profit = math.floor(totalCatch)/2
-		exports.global:sendLocalMeAction(thePlayer,"sells" .. totalCatch .."lbs of fish.")
-		exports.global:givePlayerSafeMoney(thePlayer, profit)
-		outputChatBox("You made $".. profit .." from the fish you caught." ,thePlayer, 255, 104, 91)
-		totalCatch = 0
-		destroyElement(blip)
-		destroyElement(marker)
-		destroyElement(colsphere)
-		blip=nil
-		marker=nil
-		colsphere=nil
+		if (totalCatch == 0) then
+			outputChatBox("You need to catch some fish to sell first." ,thePlayer, 255, 0, 0)
+		else
+			local profit = math.floor(totalCatch)/2
+			exports.global:sendLocalMeAction(thePlayer,"sells " .. totalCatch .."lbs of fish.")
+			exports.global:givePlayerSafeMoney(thePlayer, profit)
+			outputChatBox("You made $".. profit .." from the fish you caught." ,thePlayer, 255, 104, 91)
+			totalCatch = 0
+			destroyElement(blip)
+			destroyElement(marker)
+			destroyElement(colsphere)
+			blip=nil
+			marker=nil
+			colsphere=nil
+		end
 	else
-		outputChatBox("You need to be at the fish mongers to sell your catch.", thePlayer, 255, 0, 0)
+		outputChatBox("You need to be at the fish market to sell your catch.", thePlayer, 255, 0, 0)
 	end
 end
 addCommandHandler("sellFish", unloadCatch, false, false)

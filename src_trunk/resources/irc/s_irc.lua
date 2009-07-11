d37f145d0afc1b8ca59b1a24624f8aef69889277
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-11 17:09:03 +0000

    Changes to irc system
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@459 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/irc/s_irc.lua
+++ src_trunk/resources/irc/s_irc.lua
@@ -1,15 +1,20 @@
 server = "irc.gtanet.com"
 port = 6667
-username = "VGMTAServer"
+username = "ValhallaGaming"
 channel = "#vgmta.admins"
+pubchannel = "#mta"
 password = "adminmtavg"
 
 conn = nil
+timer = nil
 
 function initIRC()
 	ircInit()
 	conn = ircOpen(server, port, username, channel, password)
 	sendMessage("Server Started.")
+	displayStatus()
+	timer = setTimer(displayStatus, 600000, 0)
+	ircJoin(conn, pubchannel)
 end
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), initIRC)
 
@@ -17,6 +22,8 @@ function stopIRC()
 	sendMessage("Server Stopped.")
 	ircPart(conn, channel)
 	ircDisconnect(conn)
+	killTimer(timer)
+	timer = nil
 	conn = nil
 end
 addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), stopIRC)
@@ -44,4 +51,23 @@ function sendMessage(message)
 	
 	outputDebugString(tostring(message))
 	ircMessage(conn, channel, "[" .. hour .. ":" .. mins .. "] " .. tostring(message))
+end
+
+function displayStatus()
+	local playerCount = getPlayerCount()
+	local maxPlayers = getMaxPlayers()
+	local servername = getServerName()
+	local ip = "67.210.235.106:22003"
+		
+	local totalping = 0
+	local averageping = 0
+	
+	for key, value in ipairs(exports.pool:getPoolElementsByType("player")) do
+		totalping = totalping + getPlayerPing(value)
+	end
+	
+	averageping = math.floor(totalping / playerCount)
+		
+	local output = servername .. " - " .. playerCount .. "/" .. maxPlayers .. "(" .. math.ceil((playerCount/maxPlayers)*100) .. "%) - " .. ip .. " - GameMode: Roleplay - Average Ping: " .. averageping .. "."
+	ircMessage(conn, channel, output)
 end

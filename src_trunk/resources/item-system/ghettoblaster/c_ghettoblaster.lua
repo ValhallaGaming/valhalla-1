db31f7b4ae15bb4d156f53d04a41fd36637786f1
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-13 05:32:16 +0000

    You can now have unlimited ghettoblasters
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@501 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/item-system/ghettoblaster/c_ghettoblaster.lua
+++ src_trunk/resources/item-system/ghettoblaster/c_ghettoblaster.lua
@@ -1,29 +1,17 @@
-sound = nil
-blaster = nil
-ox, oy, oz = nil
+blasters = { }
 
 function elementStreamIn()
 	if (getElementType(source)=="object") then
 		local model = getElementModel(source)
 		if (model==2226) then
 			local x, y, z = getElementPosition(source)
-			local tx, ty, tz = getElementPosition(getLocalPlayer())
 			
-			if (blaster==nil) or (getDistanceBetweenPoints3D(x, y, z, tx, ty, tz)<getDistanceBetweenPoints3D(x, y, z, ox, oy, oz)) then
-				ox, oy, oz = x, y, z
-				
-				if (blaster) then
-					blaster = nil
-					stopSound(sound)
-					sound = nil
-				end
-				blaster = source
-				sound = playSound3D("ghettoblaster/loop.mp3", x, y, z, true)
-				setSoundMaxDistance(sound, 20)
-				
-				if (isPedInVehicle(getLocalPlayer())) then
-					setSoundVolume(sound, 0.5)
-				end
+			local sound = playSound3D("ghettoblaster/loop.mp3", x, y, z, true)
+			blasters[source] = sound
+			setSoundMaxDistance(sound, 20)
+			
+			if (isPedInVehicle(getLocalPlayer())) then
+				setSoundVolume(sound, 0.5)
 			end
 		end
 	end
@@ -31,27 +19,25 @@ end
 addEventHandler("onClientElementStreamIn", getRootElement(), elementStreamIn)
 
 function elementStreamOut()
-	if (blaster~=nil) then
-		if (source==blaster) then
-			blaster = nil
-			stopSound(sound)
-			sound = nil
-		end
+	if (blasters[source]~=nil) then
+		local sound = blasters[source]
+		stopSound(sound)
+		blasters[source] = nil
 	end
 end
 addEventHandler("onClientElementStreamOut", getRootElement(), elementStreamOut)
 addEventHandler("onClientElementDestroy", getRootElement(), elementStreamOut)
 
 function dampenSound(thePlayer)
-	if (thePlayer==getLocalPlayer()) and (isElement(sound)) then
-		setSoundVolume(sound, 0.5)
+	for key, value in pairs(blasters) do
+		setSoundVolume(value, 0.5)
 	end
 end
 addEventHandler("onClientVehicleEnter", getRootElement(), dampenSound)
 
 function boostSound(thePlayer)
-	if (thePlayer==getLocalPlayer()) and (isElement(sound)) then
-		setSoundVolume(sound, 1.0)
+	for key, value in pairs(blasters) do
+		setSoundVolume(value, 1.0)
 	end
 end
 addEventHandler("onClientVehicleExit", getRootElement(), boostSound)

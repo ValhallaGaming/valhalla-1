2ebf0275bdb4da9566d0a011544f44fd865c3abe
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-13 00:06:40 +0000

    Car alarm effect
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@481 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

---
+++ src_trunk/resources/realism-system/s_alarm.lua
@@ -0,0 +1,24 @@
+function pickLock(thePlayer)
+	if(isVehicleLocked(source))then
+		CarAlarm(source, thePlayer)
+	end
+end
+addEventHandler("onVehicleStartEnter", getRootElement(), pickLock)
+
+function CarAlarm(theVehicle, thePlayer)
+	if ( getVehicleOverrideLights ( theVehicle ) ~= 2 ) then  -- if the current state isn't 'force on'
+		setVehicleOverrideLights ( theVehicle, 2 )            -- force the lights on
+		
+	else
+		setVehicleOverrideLights ( theVehicle, 1 )            -- otherwise, force the lights off
+		
+	end
+	local carX, carY, carZ = getElementPosition( theVehicle )
+	local alarmSphere = createColSphere( carX, carY, carZ, 200 )
+	exports.pool:allocateElement(alarmSphere) -- Create the colSphere for chat output to local players
+	local targetPlayers = getElementsWithinColShape( alarmSphere, "player" )
+	for i, key in ipairs( targetPlayers ) do
+		playSoundFrontEnd(key, 2)
+	end
+	destroyElement (alarmSphere)
+end

375834fa87f782690c14d8443754fc5609112e7c
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-14 16:35:04 +0000

    0000619: Grammatical error in the "Katana" description
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@545 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/shop-system/c_generalshop_system.lua
+++ src_trunk/resources/shop-system/c_generalshop_system.lua
@@ -234,7 +234,7 @@ function getItemsForSale(shop_type, race)
 	
 		-- guns --
 		item[1] = {"Brass Knuckles","A pair of brass knuckles, ouch.", "100", 1, 1, 1,true,50}
-		item[2] = {"Katana", "You're favourite Japanese sword.", "200", 8, 1, 1,true,100}
+		item[2] = {"Katana", "Your favourite Japanese sword.", "200", 8, 1, 1,true,100}
 		item[3] = {"9mm Pistol", "A silver, 9mm handgun, comes with 100 ammo.", "250",  22, 100, 1,true,200}
 		item[4] = {"Shotgun", "A silver shotgun - comes with 30 ammo.", "450", 25, 30, 1,true,400}
 		--item[5] = {"Uzi", "A small micro-uzi - comes with 250 ammo.", "450", 28, 250, 1,true,190}

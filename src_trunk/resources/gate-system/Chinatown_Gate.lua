8afcdf062eea4ed034043e3f0a3911809d9db1a1
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-17 10:23:16 +0000

    Added gate in china town.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@585 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

---
+++ src_trunk/resources/gate-system/Chinatown_Gate.lua
@@ -0,0 +1,39 @@
+local objChinaGate = createObject(969, 2562.5, 1822, 10, 0, 0, 0)
+exports.pool:allocateElement(objChinaGate)
+
+local objChinaFence = createObject( 983, 2543.97, 1839.34, 10.5, 0, 0, 0)
+exports.pool:allocateElement(objChinaFence)
+
+open = false
+
+-- Gate code
+function useChinaGate(thePlayer)
+	local team = getPlayerTeam(thePlayer)
+	
+	if (team==getTeamFromName("Yamaguchi Gumi")) then
+		local x, y, z = getElementPosition(thePlayer)
+		local distance = getDistanceBetweenPoints3D( x, y, z)
+
+		if (distance<=10) and (open==false) then
+			open = true
+			outputChatBox("Yakuza gate is now Open!", thePlayer, 0, 255, 0)
+			moveObject(objChinaGate, 3000, 2568, 1822, 10, 0, 0, 0)
+			setTimer(closeChinaGate, 5000, 1, thePlayer)
+		end
+	end
+end
+addCommandHandler("gate", useChinaGate)
+
+function closeChinaGate(thePlayer)
+	if (getElementType(thePlayer)) then
+		outputChatBox("Yakuza gate is now Closed!", thePlayer, 255, 0, 0)
+	end
+	
+	moveObject(objChinaGate, 3000, 2562.5, 1822, 10, 0, 0, 0,)
+	setTimer(resetChinaGateState, 1000, 1)
+end
+
+
+function resetChinaGateState()
+	open = false
+end

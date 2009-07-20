d431982c69d8cb85b8e8249414854794818712df
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-20 21:12:14 +0000

    Clientside optomization
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@660 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/lvpd-system/c_lasers.lua
+++ src_trunk/resources/lvpd-system/c_lasers.lua
@@ -1,37 +1,39 @@
 function showLaser()
 	for key, value in ipairs(getElementsByType("player")) do
-		local weapon = getPedWeapon(value)
-		
-		if (weapon==24 or weapon==29 or weapon==31 or weapon==34) then
-			local laser = getElementData(value, "laser")
+		if (isElement(value)) then
+			local weapon = getPedWeapon(value)
 			
-			if (laser==1) then
-				local sx, sy, sz = getPedTargetStart(value)
-				local ex, ey, ez = getPedTargetEnd(value)
-				local task = getPedTask(value, "secondary", 0)
+			if (weapon==24 or weapon==29 or weapon==31 or weapon==34) then
+				local laser = getElementData(value, "laser")
+				
+				if (laser==1) then
+					local sx, sy, sz = getPedTargetStart(value)
+					local ex, ey, ez = getPedTargetEnd(value)
+					local task = getPedTask(value, "secondary", 0)
 
-				if (task=="TASK_SIMPLE_USE_GUN") then --(task=="TASK_SIMPLE_USE_GUN" or isPedDoingGangDriveby(value)) then
-					local collision, cx, cy, cz, element = processLineOfSight(sx, sy, sz, ex, ey, ez, true, true, true, true, true, false, false, false)
-					
-					if not (collision) then
-						cx = ex
-						cy = ey
-						cz = ez
-					end
-					
-					if (isPedDucked(value)) then
-						if (value==getLocalPlayer()) then
-							local sx, sy, sz = getPedBonePosition(value, 35)
-							dxDrawLine3D(sx, sy, sz, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
-						else
-							dxDrawLine3D(sx, sy, sz-0.4, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+					if (task=="TASK_SIMPLE_USE_GUN") then --(task=="TASK_SIMPLE_USE_GUN" or isPedDoingGangDriveby(value)) then
+						local collision, cx, cy, cz, element = processLineOfSight(sx, sy, sz, ex, ey, ez, true, true, true, true, true, false, false, false)
+						
+						if not (collision) then
+							cx = ex
+							cy = ey
+							cz = ez
 						end
-					else
-						if (value==getLocalPlayer()) then
-							local sx, sy, sz = getPedBonePosition(value, 35)
-							dxDrawLine3D(sx, sy, sz, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+						
+						if (isPedDucked(value)) then
+							if (value==getLocalPlayer()) then
+								local sx, sy, sz = getPedBonePosition(value, 35)
+								dxDrawLine3D(sx, sy, sz, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+							else
+								dxDrawLine3D(sx, sy, sz-0.4, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+							end
 						else
-							dxDrawLine3D(sx, sy, sz-0.1, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+							if (value==getLocalPlayer()) then
+								local sx, sy, sz = getPedBonePosition(value, 35)
+								dxDrawLine3D(sx, sy, sz, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+							else
+								dxDrawLine3D(sx, sy, sz-0.1, cx, cy, cz, tocolor(255,0,0,75), 2, false, 0)
+							end
 						end
 					end
 				end

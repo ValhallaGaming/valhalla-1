178ba6efeed8e59e61594a662b67a5afc552d6ef
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-22 14:39:33 +0000

    0000699: Tag ID 3 for TCC.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@688 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/tag-system/c_tag_system.lua
+++ src_trunk/resources/tag-system/c_tag_system.lua
@@ -95,8 +95,15 @@ function setTag(commandName, newTag)
 	else
         local newTag = tonumber(newTag)
 		if (newTag>0) and (newTag<9) then
-			setElementData(getLocalPlayer(), "tag", true, newTag)
-			outputChatBox("Tag changed to #" .. newTag .. ".", 0, 255, 0)
+			local theTeam = getPlayerTeam(getLocalPlayer())
+			local teamName = getTeamName(theTeam)
+			
+			if (teamName~="Tatum Creek Crips") and (newTag==2) then
+				outputChatBox("You are not a member of Tatum Creek Crips", 255, 0, 0)
+			else
+				setElementData(getLocalPlayer(), "tag", true, newTag)
+				outputChatBox("Tag changed to #" .. newTag .. ".", 0, 255, 0)
+			end
 		else
 			outputChatBox("Invalid value, please enter a value between 1 and 8.", 255, 0, 0)
 		end

33195882171850bfa3d7db8b233cdf3dd43864b2
Mr.Hankey <Mr.Hankey@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-24 12:25:35 +0000

    "Fixed Yakuza HQ gate" #2
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@703 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/gate-system/Chinatown_Gate.lua
+++ src_trunk/resources/gate-system/Chinatown_Gate.lua
@@ -1,7 +1,7 @@
-local objChinaGate = createObject(986, 2562.5, 1822, 10, 0, 0, 0)
+local objChinaGate = createObject(986, 2566.45, 1822, 11, 0, 0, 180)
 exports.pool:allocateElement(objChinaGate)
 
-local objChinaGate2 = createObject(986, 2562.5, 1826, 10, 0, 0, 0)
+local objChinaGate2 = createObject(986, 2566.45, 1826, 11, 0, 0, 180)
 exports.pool:allocateElement(objChinaGate2)
 
 local objChinaFence = createObject( 987, 2544.3037109375, 1840.0390625, 10, 0, 0, 0)
@@ -20,8 +20,8 @@ function useChinaGate(thePlayer)
 		if (distance<=10) and (open==false) then
 			open = true
 			outputChatBox("Yakuza gate is now Open!", thePlayer, 0, 255, 0)
-			moveObject(objChinaGate, 3000, 2568, 1822, 10, 0, 0, 0)
-            moveObject(objChinaGate2, 4000, 2568, 1826, 10, 0, 0, 0)
+			moveObject(objChinaGate, 3000, 2571.8, 1822, 11)
+            moveObject(objChinaGate2, 4000, 2571.8, 1826, 11)
 			setTimer(closeChinaGate, 6000, 1, thePlayer)
 		end
 	end
@@ -33,8 +33,8 @@ function closeChinaGate(thePlayer)
 		outputChatBox("Yakuza gate is now Closed!", thePlayer, 255, 0, 0)
 	end
 	
-	moveObject(objChinaGate, 3000, 2562.5, 1822, 10, 0, 0, 0)
-    moveObject(objChinaGate2, 4000, 2562.5, 1826, 10, 0, 0, 0) 
+	moveObject(objChinaGate, 3000, 2566.45, 1822, 11)
+    moveObject(objChinaGate2, 4000, 2566.45, 1826, 11) 
 	setTimer(resetChinaGateState, 1000, 1)
 end
 

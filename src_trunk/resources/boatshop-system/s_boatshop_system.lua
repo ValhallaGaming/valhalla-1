2e73111a50846a6d15b7e22659dd842fd149115f
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-25 23:49:46 +0000

    Boat shop fix
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@734 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/boatshop-system/s_boatshop_system.lua
+++ src_trunk/resources/boatshop-system/s_boatshop_system.lua
@@ -45,6 +45,50 @@ end
 addEvent("buyBoat", true)
 addEventHandler("buyBoat", getRootElement(), buyCar)
 
+function SmallestVehicleID( ) --  Loop which finds the smallest ID in the SQL instead of the biggest one.
+	UsedID = {}		
+	local id = 0
+	local answer = 2 -- 0 = ID = 1 . 1 =Suitable ID found. 2= Still searching for ID.
+	local highest = 0
+	local result = mysql_query(handler, "SELECT id FROM vehicles")
+	if(result) then
+		for result, row in mysql_rows(result) do
+						
+				UsedID[tonumber(row[1])] = 1
+				if (tonumber(row[1]) > highest) then
+					highest = tonumber(row[1])
+				end
+
+		end
+		
+	end
+
+	if(highest > 0) then
+		for i = 1, highest do
+			if(UsedID[i] ~= 1) then
+				answer = 1
+				id = i
+				break
+			end
+		end
+	else
+		answer = 0
+		id = 1
+
+	end
+	
+	if(answer == 2) then
+		answer = 1
+		id = highest + 1
+
+	end
+	if(answer ~= 2) then
+		return id
+	else
+		return false
+	end
+end
+
 function makeCar(thePlayer, car, cost, id, col1, col2)
 	local rx = 0
 	local ry = 0
@@ -78,15 +122,16 @@ function makeCar(thePlayer, car, cost, id, col1, col2)
 	setVehicleOverrideLights(veh, 1)
 	setVehicleEngineState(veh, false)
 	setVehicleFuelTankExplodable(veh, false)
-						
-	local query = mysql_query(handler, "INSERT INTO vehicles SET model='" .. id .. "', x='" .. x .. "', y='" .. y .. "', z='" .. z .. "', rotx='" .. rx .. "', roty='" .. ry .. "', rotz='" .. rz .. "', color1='" .. col1 .. "', color2='" .. col2 .. "', faction='-1', owner='" .. dbid .. "', plate='" .. plate .. "', currx='" .. x .. "', curry='" .. y .. "', currz='" .. z .. "', currrx='0', currry='0', currrz='" .. rz .. "', locked='" .. locked .. "'")
+	
+	local insertid = SmallestVehicleID()
+	
+	local query = mysql_query(handler, "INSERT INTO vehicles SET id='" .. insertid .. "', model='" .. id .. "', x='" .. x .. "', y='" .. y .. "', z='" .. z .. "', rotx='" .. rx .. "', roty='" .. ry .. "', rotz='" .. rz .. "', color1='" .. col1 .. "', color2='" .. col2 .. "', faction='-1', owner='" .. dbid .. "', plate='" .. plate .. "', currx='" .. x .. "', curry='" .. y .. "', currz='" .. z .. "', currrx='0', currry='0', currrz='" .. rz .. "', locked='" .. locked .. "'")
 
 	if (query) then
 		mysql_free_result(query)
-		local id = mysql_insert_id(handler)
 							
 		exports.global:givePlayerItem(thePlayer, 3, tonumber(id))
-		setElementData(veh, "dbid", tonumber(id), false)
+		setElementData(veh, "dbid", tonumber(insertid), false)
 		setElementData(veh, "fuel", 100)
 		setElementData(veh, "engine", 0, false)
 		setElementData(veh, "oldx", x, false)

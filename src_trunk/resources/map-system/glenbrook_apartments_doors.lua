3b9961235e08820c74011772a92f1fc04b4cd28c
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-25 22:39:10 +0000

    Optomized maps
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@724 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

---
+++ src_trunk/resources/map-system/glenbrook_apartments_doors.lua
@@ -0,0 +1,12 @@
+local temp = nil
+	temp = createObject(1522, 2623.6274414063, 1205.8172607422, 9.8203125, 0, 0, 89.999938964844)  
+    temp = createObject(1522, 2623.6247558594, 1207.2839355469, 9.8203125, 0, 0, 89.999938964844)  
+	
+	temp = createObject(2987, 2243.0236816406, -1159.7537841797, 1030.0028076172, 0, 0, 89.999938964844) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2239.4118652344, -1159.7399902344, 1030.0025634766, 0, 0, 270) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2239.4055175781, -1170.7235107422, 1030.0025634766, 0, 0, 270) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2225.5810546875, -1186.8601074219, 1030.0025634766, 0, 0, 180) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2209.5700683594, -1190.4899902344, 1030.0025634766, 0, 0, 0) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2194.9975585938, -1173.0205078125, 1030.0025634766, 0, 0, 89.999938964844) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2195.0227050781, -1157.7547607422, 1030.0025634766, 0, 0, 89.999938964844) setElementInterior(temp, 15) setElementDimension(temp, 523)
+    temp = createObject(2987, 2191.322265625, -1157.0067138672, 1030.0025634766, 0, 0, 270) setElementInterior(temp, 15) setElementDimension(temp, 523)

b0646834c63b2be0183e57294930472473a837b5
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-25 23:30:22 +0000

    Another object optimization
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@729 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/gate-system/pd_side_gate.lua
+++ src_trunk/resources/gate-system/pd_side_gate.lua
@@ -3,22 +3,6 @@ exports.pool:allocateElement(objGatei)
 local objGatei2 = createObject(976, 2237.5334472656, 2448.87109375, 12.8203125, 0, 0, 90)
 exports.pool:allocateElement(objGatei2)
 
--- these are just fences to stop people climbing
-o1 = createObject(987, 2237.5334472656, 2442.37109375, 9.9203125, 0, 0, 270)
-o2 = createObject(987, 2237.5334472656, 2448.37109375, 9.9203125, 0, 0, 270)
-o3 = createObject(987, 2237.5334472656, 2430.87109375, 9.9203125, 0, 0, 0)
-
-local o4 = createObject(987, 2237.5334472656, 2469.77109375, 9.9203125, 0, 0, 270)
-local o5 = createObject(987, 2237.5334472656, 2475.77109375, 9.9203125, 0, 0, 270)
-local o6 = createObject(987, 2237.5334472656, 2487.77109375, 9.9203125, 0, 0, 270)
-local o7 = createObject(987, 2237.5334472656, 2499.77109375, 9.9203125, 0, 0, 270)
-local o8 = createObject(987, 2237.5334472656, 2502.77109375, 9.9203125, 0, 0, 270)
-local p9 = createObject(987, 2249.5334472656, 2502.77109375, 9.9203125, 0, 0, 180)
-local o10 = createObject(987, 2260.5334472656, 2502.77109375, 9.9203125, 0, 0, 180)
-local o11 = createObject(987, 2271.5334472656, 2502.77109375, 9.9203125, 0, 0, 180)
-local o12 = createObject(987, 2282.5334472656, 2502.77109375, 9.9203125, 0, 0, 180)
-local o13 = createObject(987, 2293.5334472656, 2502.77109375, 9.9203125, 0, 0, 180)
-
 local open = false
 
 -- Gate code

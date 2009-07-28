47319e93564c6fc39cdcf6e2f3b43c32bf891bc2
Serizzim <Serizzim@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-28 17:48:17 +0000

    /delthisveh added to the list.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@821 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/admin-system/c_admin_help.lua
+++ src_trunk/resources/admin-system/c_admin_help.lua
@@ -104,6 +104,7 @@ function adminhelp (sourcePlayer, commandName)
 					{ "/setmoney", "/setmoney [player] [money]", "sets the players money to that value" },
 					{ "/givemoney", "/givemoney [player] [money]", "gives the player money in addition to his current cash" },
 					{ "/delveh", "/delveh [id]", "removes the vehicle with that id" }
+					{ "/delthisveh", "/delthisveh", "removes the vehicle you currently occupy" }
 				},
 				-- level 5: Head Admins
 				{

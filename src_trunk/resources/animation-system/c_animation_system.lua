1d5da00a272d401f3d674d2e5389e97c7302fba5
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-29 08:36:04 +0000

    fixed shadow for "Press Spacebar to Cancel Animation"
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@843 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/animation-system/c_animation_system.lua
+++ src_trunk/resources/animation-system/c_animation_system.lua
@@ -7,7 +7,7 @@ function onRender()
 		local screenWidth, screenHeight = guiGetScreenSize()
 		anim = true
 		dxDrawText("Press Spacebar to Cancel Animation", screenWidth-420, screenHeight-91, screenWidth, screenHeight, tocolor ( 0, 0, 0, 255 ), 1, "pricedown")
-		dxDrawText("Press Spacebar to Cancel Animation", screenHeight-165, screenHeight-93, screenWidth-30, screenHeight, tocolor ( 255, 255, 255, 255 ), 1, "pricedown")
+		dxDrawText("Press Spacebar to Cancel Animation", screenWidth-422, screenHeight-93, screenWidth-30, screenHeight, tocolor ( 255, 255, 255, 255 ), 1, "pricedown")
 	end
 	
 	if not (getPedAnimation(getLocalPlayer())) and (anim) then

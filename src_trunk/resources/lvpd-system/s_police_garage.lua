c465a750b89804f0f4810df6b34437ed26bc6340
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-31 01:06:03 +0000

    Updated a coord
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@903 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/lvpd-system/s_police_garage.lua
+++ src_trunk/resources/lvpd-system/s_police_garage.lua
@@ -1,6 +1,6 @@
-local marker1 = createMarker(2240.46484375, 2447.16796875, 3.2734375, "cylinder", 5, 255, 194, 14, 150)
+local marker1 = createMarker(1545.55859375, -1659.109375, 5.890625, "cylinder", 5, 255, 194, 14, 150)
 exports.pool:allocateElement(marker1)
-local marker2 = createMarker(2240.46484375, 2456.6247558594, 3.2734375, "cylinder", 5, 255, 181, 165, 213)
+local marker2 = createMarker(1545.55859375, -1659.109375, 5.890625, "cylinder", 5, 255, 181, 165, 213)
 exports.pool:allocateElement(marker2)
 
 -- Nice little guard ped

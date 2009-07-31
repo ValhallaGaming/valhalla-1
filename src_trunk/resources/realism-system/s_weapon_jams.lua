74e16fb0e24adcd6435c1ee5535a7671d9a062e3
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-31 02:34:40 +0000

    Opened all garage gates
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@913 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/s_weapon_jams.lua
+++ src_trunk/resources/realism-system/s_weapon_jams.lua
@@ -13,6 +13,11 @@ function resourceStart(res)
 		for key, value in ipairs(exports.pool:getPoolElementsByType("player")) do
 			toggleControl(value, "fire", true)
 		end
+		
+		-- garage fix
+		for i = 1, 50 do
+			setGarageOpen(i-1, true)
+		end
 	end
 end
 addEventHandler("onResourceStart", getRootElement(), resourceStart)

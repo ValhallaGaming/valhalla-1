71cfac01408862427b47ab323ce0b0c62af87993
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-07-31 22:05:37 +0000

    Fix for vehicles ramming Smokes wall
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@929 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/map-system/smokes_house_wall.lua
+++ src_trunk/resources/map-system/smokes_house_wall.lua
@@ -1,1 +1,5 @@
-createObject(3059, 2522.05, -1272.9325, 35.589998626709)
+createObject(3059, 2522.05, -1272.9325, 35.589998626709) -- wall
+local hack = createObject(976, 2521.5, -1279, 34.089998626709, 0, 0, 90) -- Hack to stop people driving through it
+local hack2 = createObject(976, 2521.5, -1276, 34.089998626709, 0, 0, 90)
+setElementAlpha(hack, 0)
+setElementAlpha(hack2, 0)

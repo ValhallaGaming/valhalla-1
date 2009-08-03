ec4a04fd7bb3e83169ff3042cb47c7ea09269b96
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-03 22:48:11 +0000

    fixed impound gui showing multiple times
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@993 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/tow-system/c_towing_system.lua
+++ src_trunk/resources/tow-system/c_towing_system.lua
@@ -9,42 +9,44 @@ local vehElements = {}
 car, wImpound, bClose, bRelease, gCars, lCost, IDcolumn = nil
 
 function showImpoundUI(vehElementsret)
-	local width, height = 400, 200
-	local scrWidth, scrHeight = guiGetScreenSize()
-	local x = scrWidth - width
-	local y = scrHeight/10
-	
-	wImpound = guiCreateWindow(x, y, width, height, "Impound: Release a vehicle", false)
-	guiWindowSetSizable(wImpound, false)
-	
-	bClose = guiCreateButton(0.6, 0.85, 0.2, 0.1, "Close", true, wImpound)
-	bRelease = guiCreateButton(0.825, 0.85, 0.2, 0.1, "Release", true, wImpound)
-	addEventHandler("onClientGUIClick", bClose, hideImpoundUI, false)
-	addEventHandler("onClientGUIClick", bRelease, releaseCar, false)
-	
-
-	gCars = guiCreateGridList(0.05, 0.1, 0.9, 0.65, true, wImpound)
-	addEventHandler("onClientGUIClick", gCars, updateCar, false)
-	local col = guiGridListAddColumn(gCars, "Impounded Vehicles", 0.7)
-	IDcolumn = guiGridListAddColumn(gCars, "ID", 0.2)
-	vehElements = vehElementsret
-	for key, value in ipairs(vehElements) do
-		local dbid = getElementData(value, "dbid")
-		local row = guiGridListAddRow(gCars)
-		guiGridListSetItemText(gCars, row, col, getVehicleName(value), false, false)
-		guiGridListSetItemText(gCars, row, IDcolumn, tostring(dbid), false, false)
-	end
-	guiGridListSetSelectedItem(gCars, 0, 1)
-	
-	lCost = guiCreateLabel(0.3, 0.85, 0.2, 0.1, "Cost: 75$", true, wImpound)
-	guiSetFont(lCost, "default-bold-small")
-	
-	updateCar()
+	if not wImpound then
+		local width, height = 400, 200
+		local scrWidth, scrHeight = guiGetScreenSize()
+		local x = scrWidth - width
+		local y = scrHeight/10
+		
+		wImpound = guiCreateWindow(x, y, width, height, "Impound: Release a vehicle", false)
+		guiWindowSetSizable(wImpound, false)
+		
+		bClose = guiCreateButton(0.6, 0.85, 0.2, 0.1, "Close", true, wImpound)
+		bRelease = guiCreateButton(0.825, 0.85, 0.2, 0.1, "Release", true, wImpound)
+		addEventHandler("onClientGUIClick", bClose, hideImpoundUI, false)
+		addEventHandler("onClientGUIClick", bRelease, releaseCar, false)
 		
 
-	guiSetInputEnabled(true)
-	
-	outputChatBox("Welcome to The Impound lot.")
+		gCars = guiCreateGridList(0.05, 0.1, 0.9, 0.65, true, wImpound)
+		addEventHandler("onClientGUIClick", gCars, updateCar, false)
+		local col = guiGridListAddColumn(gCars, "Impounded Vehicles", 0.7)
+		IDcolumn = guiGridListAddColumn(gCars, "ID", 0.2)
+		vehElements = vehElementsret
+		for key, value in ipairs(vehElements) do
+			local dbid = getElementData(value, "dbid")
+			local row = guiGridListAddRow(gCars)
+			guiGridListSetItemText(gCars, row, col, getVehicleName(value), false, false)
+			guiGridListSetItemText(gCars, row, IDcolumn, tostring(dbid), false, false)
+		end
+		guiGridListSetSelectedItem(gCars, 0, 1)
+		
+		lCost = guiCreateLabel(0.3, 0.85, 0.2, 0.1, "Cost: 75$", true, wImpound)
+		guiSetFont(lCost, "default-bold-small")
+		
+		updateCar()
+			
+
+		guiSetInputEnabled(true)
+		
+		outputChatBox("Welcome to The Impound lot.")
+	end
 end
 
 function updateCar()

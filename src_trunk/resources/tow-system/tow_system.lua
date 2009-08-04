b9dba6eaf3750070f97ee1d4724949bad0e7b79a
Cazomino05 <Cazomino05@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-04 02:01:19 +0000

    Fixed civ release
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1001 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/tow-system/tow_system.lua
+++ src_trunk/resources/tow-system/tow_system.lua
@@ -207,7 +207,10 @@ function updateCivilianVehicles(theTruck)
 		local owner = getElementData(source, "owner")
 		local faction = getElementData(source, "faction")
 		local dbid = getElementData(source, "dbid")
-		if (dbid >= 0 and faction == -1 and owner == -1) then
+		outputChatBox(dbid)
+		outputChatBox(faction)
+		outputChatBox(owner)
+		if (dbid >= 0 and faction == -1 and owner < 0) then
 			call(getResourceFromName("faction-system"), "addToFactionMoney", 24, 75)
 			outputChatBox("The state has un-impounded the vehicle you where towing.", getVehicleOccupant(theTruck), 255, 194, 14)
 			respawnVehicle(source)

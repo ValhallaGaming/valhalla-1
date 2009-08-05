9b5914953a6326f05a9f1cdabf2ea3cd39ec36a2
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-05 18:06:22 +0000

    other donator fix
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1041 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/global/c_donator_globals.lua
+++ src_trunk/resources/global/c_donator_globals.lua
@@ -86,7 +86,7 @@ function cgetPlayerDonatorTitle(thePlayer)
 		return "Gold Donator"
 	elseif (donatorLevel==4) then
 		return "Platinum Donator"
-	elseif (donatornLevel==5) then
+	elseif (donatorLevel==5) then
 		return "Pearl Donator"
 	elseif (donatorLevel==6) then
 		return "Diamond Donator"

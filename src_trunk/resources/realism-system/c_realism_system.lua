6dc0ed2ccd2ddb7d6fb788a3a705525bc8f1056f
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-06 14:54:39 +0000

    Added strippers to new strip club. Reduced all strippers to two timers.
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1057 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_realism_system.lua
+++ src_trunk/resources/realism-system/c_realism_system.lua
@@ -31,92 +31,84 @@ function realisticWeaponSounds(weapon)
 end
 addEventHandler("onClientPlayerWeaponFire", getRootElement(), realisticWeaponSounds)
 
+----------------------------------------------------
+------------------- STRIPPER PEDS ------------------
+----------------------------------------------------
+
 ------/////////////////////// Pig Pen Strippers ///////////////////////------
--- Stripper Isla View
-local islaView = createPed (152, 1216.30, -6.45, 1000.32)
-setElementInterior (islaView, 2)
-setElementDimension (islaView, 1197)
-setPedAnimation ( islaView, "STRIP", "STR_C1", -1, true, false, false)
-local islaDanceNumber = 1
+local pigpenDancer1=createPed(152,1216.30,-6.45,1000.32)
+setElementInterior(pigpenDancer1,2)
+setElementDimension(pigpenDancer1,1197)
+setPedAnimation(pigpenDancer1,"STRIP","STR_C1",-1,true,false,false)
 
--- Isla Dance Cycle
-function islaDance()
-	if (islaDanceNumber == 1) then
-		setPedAnimation ( islaView, "STRIP", "STR_Loop_C", -1, true, false, false)
-		islaDanceNumber = islaDanceNumber + 1
-	elseif (islaDanceNumber == 2) then
-		setPedAnimation ( islaView, "STRIP", "strip_D", -1, true, false, false)
-		islaDanceNumber = islaDanceNumber + 1
-	elseif (islaDanceNumber == 3) then
-		setPedAnimation ( islaView, "STRIP", "STR_C1", -1, true, false, false)
-		islaDanceNumber = islaDanceNumber - 2 -- reset to 1
+local pigpenDancer2=createPed (257,1208.12,-6.05,1000.32)
+setPedAnimation(pigpenDancer2,"STRIP","STR_C2",-1,true,false,false)
+setElementInterior(pigpenDancer2,2)
+setElementDimension(pigpenDancer2,1197)
+
+------/////////////////////// East Beach Strippers ///////////////////////------
+local eastBeachDancer1=createPed(152,1216.30,-6.45,1000.32)
+setElementInterior(eastBeachDancer1,2)
+setElementDimension(eastBeachDancer1,1301)
+setPedAnimation(eastBeachDancer1,"STRIP","STR_C1",-1,true,false,false)
+
+local eastBeachDancer2=createPed(257,1208.12,-6.05,1000.32)
+setPedAnimation(eastBeachDancer2,"STRIP","STR_C2",-1,true,false,false)
+setElementInterior(eastBeachDancer2,2)
+setElementDimension(eastBeachDancer2,1301)
+
+------/////////////////////// Ganton Strippers ///////////////////////------
+local gantonDancer1=createPed(152,1216.30,-6.45,1000.32)
+setElementInterior(gantonDancer1,2)
+setElementDimension(gantonDancer1,1301)
+setPedAnimation(gantonDancer1,"STRIP","STR_C1",-1,true,false,false)
+
+local gantonDancer2=createPed(257,1208.12,-6.05,1000.32)
+setPedAnimation(gantonDancer2,"STRIP","STR_C2",-1,true,false,false)
+setElementInterior(gantonDancer2,2)
+setElementDimension(gantonDancer2,1301)
+
+local DancerOneDanceNumber=1
+local DancerTwoDanceNumber=1
+
+-- Dancer 1 Cycle
+function dancerOneLoop()
+	if(DancerOneDanceNumber==1)then
+		setPedAnimation(pigpenDancer1,"STRIP","STR_Loop_C",-1,true,false,false)
+		setPedAnimation(eastBeachDancer1,"STRIP","STR_Loop_C",-1,true,false,false)
+		setPedAnimation(gantonDancer1,"STRIP","STR_Loop_C",-1,true,false,false)
+		DancerOneDanceNumber=DancerOneDanceNumber+1
+	elseif(DancerOneDanceNumber==2)then
+		setPedAnimation(pigpenDancer1,"STRIP","strip_D",-1,true,false,false)
+		setPedAnimation(eastBeachDancer1,"STRIP","strip_D",-1,true,false,false)
+		setPedAnimation(gantonDancer1,"STRIP","strip_D",-1,true,false,false)
+		DancerOneDanceNumber=DancerOneDanceNumber+1
+	elseif(DancerOneDanceNumber==3)then
+		setPedAnimation(pigpenDancer1,"STRIP","STR_C1",-1,true,false,false)
+		setPedAnimation(eastBeachDancer1,"STRIP","STR_C1",-1,true,false,false)
+		setPedAnimation(gantonDancer1,"STRIP","STR_C1",-1,true,false,false)
+		DancerOneDanceNumber=1
 	end
 end
-islaTimer = setTimer ( islaDance, 15000, 0)
+local dancerOneTimer=setTimer(dancerOneLoop,15000,0)
 
--- Stripper Angel Rain	
-local angelRain = createPed (257, 1208.12, -6.05, 1000.32)
-setPedAnimation ( angelRain, "STRIP", "STR_C2", -1, true, false, false)
-setElementInterior (angelRain, 2)
-setElementDimension (angelRain, 1197)
-local angelDanceNumber = 1
-
--- Angel Dance Cycle
-function angelDance()
-	if (angelDanceNumber == 1) then
-		setPedAnimation ( angelRain, "STRIP", "Strip_Loop_B", -1, true, false, false)
-		angelDanceNumber = angelDanceNumber + 1
-	elseif (angelDanceNumber == 2) then
-		setPedAnimation ( angelRain, "STRIP", "STR_Loop_A", -1, true, false, false)
-		angelDanceNumber = angelDanceNumber + 1
-	elseif (angelDanceNumber == 3) then
-		setPedAnimation ( angelRain, "STRIP", "STR_C2", -1, true, false, false)
-		angelDanceNumber = angelDanceNumber - 1 -- reset to 1
+-- Dancer 2 Cycle
+function dancerTwoLoop()
+	if (DancerTwoDanceNumber == 1) then
+		setPedAnimation(pigpenDancer2,"STRIP","Strip_Loop_B",-1,true,false,false)
+		setPedAnimation(eastBeachDancer2,"STRIP","Strip_Loop_B",-1,true,false,false)
+		setPedAnimation(gantonDancer2,"STRIP","Strip_Loop_B",-1,true,false,false)
+		DancerTwoDanceNumber=DancerTwoDanceNumber+1
+	elseif(DancerTwoDanceNumber==2)then
+		setPedAnimation(pigpenDancer2,"STRIP","STR_Loop_A",-1,true,false,false)
+		setPedAnimation(eastBeachDancer2,"STRIP","STR_Loop_A",-1,true,false,false)
+		setPedAnimation(gantonDancer2,"STRIP","STR_Loop_A",-1,true,false,false)
+		DancerTwoDanceNumber=DancerTwoDanceNumber+1
+	elseif(DancerTwoDanceNumber==3)then
+		setPedAnimation(pigpenDancer2,"STRIP","STR_C2",-1,true,false,false)
+		setPedAnimation(eastBeachDancer2,"STRIP","STR_C2",-1,true,false,false)
+		setPedAnimation(gantonDancer2,"STRIP","STR_C2",-1,true,false,false)
+		DancerTwoDanceNumber=1
 	end
 end
-angelTimer = setTimer ( angelDance, 12000, 0)
-
-------/////////////////////// Dragon Lady strippers ///////////////////////------
--- Stripper 1
-local DrgStrip1 = createPed (140, 1216.30, -6.45, 1000.32)
-setElementInterior (DrgStrip1, 2)
-setElementDimension (DrgStrip1, 685)
-setPedAnimation ( DrgStrip1, "STRIP", "STR_C1", -1, true, false, false)
-local DrgStrip1DanceNumber = 1
-
--- Stripper 1 Cycle
-function DrgStrip1Dance()
-	if (DrgStrip1DanceNumber == 1) then
-		setPedAnimation ( DrgStrip1, "STRIP", "STR_C1", -1, true, false, false)
-		DrgStrip1DanceNumber = DrgStrip1DanceNumber + 1
-	elseif (DrgStrip1DanceNumber == 2) then
-		setPedAnimation ( DrgStrip1, "STRIP", "strip_D", -1, true, false, false)
-		DrgStrip1DanceNumber = DrgStrip1DanceNumber + 1
-	elseif (DrgStrip1DanceNumber == 3) then
-		setPedAnimation ( DrgStrip1, "STRIP", "strip_G", -1, true, false, false)
-		DrgStrip1DanceNumber = DrgStrip1DanceNumber - 2 -- reset to 1
-	end
-end
-DrgStrip1Timer = setTimer ( DrgStrip1Dance, 15000, 0)
-
--- Stripper 2	
-local DrgStrip2 = createPed (257, 1208.12, -6.05, 1000.32)
-setPedAnimation ( DrgStrip2, "STRIP", "STR_Loop_A", -1, true, false, false)
-setElementInterior (DrgStrip2, 2)
-setElementDimension (DrgStrip2, 685)
-local DrgStrip2DanceNumber = 1
-
--- Stripper 2 Cycle
-function DrgStrip2Dance()
-	if (DrgStrip2DanceNumber == 1) then
-		setPedAnimation ( DrgStrip2, "STRIP", "Strip_Loop_B", -1, true, false, false)
-		DrgStrip2DanceNumber = DrgStrip2DanceNumber + 1
-	elseif (DrgStrip2DanceNumber == 2) then
-		setPedAnimation ( DrgStrip2, "STRIP", "STR_Loop_A", -1, true, false, false)
-		DrgStrip2DanceNumber = DrgStrip2DanceNumber + 1
-	elseif (DrgStrip2DanceNumber == 3) then
-		setPedAnimation ( DrgStrip2, "STRIP", "STR_C2", -1, true, false, false)
-		DrgStrip2DanceNumber = DrgStrip2DanceNumber - 1 -- reset to 1
-	end
-end
-DrgStrip2Timer = setTimer ( DrgStrip2Dance, 12000, 0)
+local dancerTwoTimer=setTimer(dancerTwoLoop,12000,0)

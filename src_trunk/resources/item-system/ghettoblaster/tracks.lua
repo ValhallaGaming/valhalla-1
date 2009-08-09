e1b84e4a0ae8a88a6e204180574050b1564cdf2f
Chamberlain <Chamberlain@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-09 11:37:52 +0000

    Ghetto blaster loops
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1105 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/item-system/ghettoblaster/tracks.lua
+++ src_trunk/resources/item-system/ghettoblaster/tracks.lua
@@ -1,5 +1,7 @@
 tracks =
 {
-	{ file="loop.mp3" },
-	{ file="187_loop.mp3" }
+	{ file="cali_vacation_loop.mp3" },
+	{ file="whats_my_name_loop.mp3" }
+	{ file="what_we_do_loop.mp3" }
+	{ file="mo_money_mo_problems_loop.mp3" }
 }

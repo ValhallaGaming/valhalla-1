262fc0565964cefe4e9ff8c62217a70893f94f50
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-22 19:00:20 +0000

    fixed a "typo" (the one who did it is to be bant on sight)
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1400 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/help-system/c_credits.lua
+++ src_trunk/resources/help-system/c_credits.lua
@@ -1,6 +1,6 @@
 function showCredits()
 	outputChatBox("------------------------------------------", 255, 194, 14)
-	outputChatBox("Script by Daniels, Chamberlain, Hankey, Cazomino05, Mabako, Carlo, Serizzim, Konstantine & Flobu.", 255, 194, 14)
+	outputChatBox("Script by Daniels, Chamberlain, Hankey, Cazomino05, mabako, Carlo, Serizzim, Konstantine & Flobu.", 255, 194, 14)
 
 	outputChatBox("------------------------------------------", 255, 194, 14)
 end

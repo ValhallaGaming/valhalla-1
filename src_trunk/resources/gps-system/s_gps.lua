c037c2c73e7300d58b729a2d032d71967e808384
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-08-28 01:38:33 +0000

    GPS can only be used in drivers seat now
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1510 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/gps-system/s_gps.lua
+++ src_trunk/resources/gps-system/s_gps.lua
@@ -18,8 +18,12 @@ function showGPS(player)
 	local vehicle = getPedOccupiedVehicle(player)
 
 	if (vehicle) then -- In vehicle
-		if (exports.global:hasItem(vehicle, 67, nil)) then -- has GPS?
-			triggerClientEvent(player, "displayGPS", vehicle)
+		local seat = getPedOccupiedVehicleSeat(player)
+		
+		if (seat==0) then
+			if (exports.global:hasItem(vehicle, 67, nil)) then -- has GPS?
+				triggerClientEvent(player, "displayGPS", vehicle)
+			end
 		end
 	end
 end

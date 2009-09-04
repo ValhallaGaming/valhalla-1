1707347788b27b5c35bad86691c5caac47b2a788
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-09-04 02:13:54 +0000

    FPS fix for head movement
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1641 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_headbob.lua
+++ src_trunk/resources/realism-system/c_headbob.lua
@@ -3,23 +3,11 @@ function bobHead()
 	local logged = getElementData(getLocalPlayer(), "loggedin")
 	
 	if (logged==1) then
-		for key, value in ipairs(getElementsByType("player")) do
-            if value == getLocalPlayer() then
-                local scrWidth, scrHeight = guiGetScreenSize()
-        		local sx = scrWidth/2
-        		local sy = scrHeight/2
-        		local x, y, z = getWorldFromScreenPosition(sx, sy, 10)
-        		setPedLookAt(value, x, y, z, 3000)
-            else
-                --local rot = getPedCameraRotation(value)
-				--local rott = getPedRotation(value)
-    			--local x, y, z = getElementPosition(value)
-    			--local vx = x + math.sin(math.rad(rot)) * 5
-    			--local vy = y + math.cos(math.rad(rot)) * 5
-
-    			--setPedLookAt(value, vx, vy, z)
-			end
-		end
+        local scrWidth, scrHeight = guiGetScreenSize()
+        local sx = scrWidth/2
+        local sy = scrHeight/2
+        local x, y, z = getWorldFromScreenPosition(sx, sy, 10)
+        setPedLookAt(value, x, y, z, 3000)
 	end
 end
 addEventHandler("onClientRender", getRootElement(), bobHead)

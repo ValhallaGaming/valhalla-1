94d5b4e104afacba704c6d772bcd7e5e4aac83db
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-09-05 03:52:03 +0000

    Weapons on back code now syncs absolutely no data
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1652 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_weapons_back.lua
+++ src_trunk/resources/realism-system/c_weapons_back.lua
@@ -1,43 +1,58 @@
-local objectmade = false
-local crouched = nil
-local weapon = nil
+weapons = { }
 
-
-
-function weaponSwitched()
-	-- SLOT 5
+function weaponSwitch(prevSlot, newSlot)
+	local weapon = getPlayerWeapon(source, prevSlot)
 	
-	if (getCameraTarget()~=getLocalPlayer()) and (objectmade) then
-		objectmade = false
-		triggerServerEvent("destroyWeaponBackModel", getLocalPlayer())
-		return
+	if (weapons[source] == nil) then
+		weapons[source] = { }
 	end
 	
-	if (getPedWeaponSlot(getLocalPlayer())~=5 and getPedWeapon(getLocalPlayer(), 5)~=0 and (getPedTotalAmmo(getLocalPlayer(), 5)>0)) and not (isPedInVehicle(getLocalPlayer())) then
-		local weap = getPedWeapon(getLocalPlayer(), 5)
-		if (weap==30 or weap==31) then
-			if not (objectmade) or (isPedDucked(getLocalPlayer())~=crouched) or (weap~=weapon) then -- no object, so lets create it
-				local bx, by, bz = getPedBonePosition(getLocalPlayer(), 3)
-				local x, y, z = getElementPosition(getLocalPlayer())
-				local r = getPedRotation(getLocalPlayer())
-				weapon = weap
-				crouched = isPedDucked(getLocalPlayer())
-				
-				local ox, oy, oz = bx-x-0.13, by-y-0.25, bz-z+0.25
-				
-				if (crouched) then
-					oz = -0.025
-				end
-				
-				triggerServerEvent("createWeaponBackModel", getLocalPlayer(), ox, oy, oz, weapon)
-				objectmade = true
-			end
+	if (weapon == 30 or weapon == 31) then
+		if (weapons[source][1] == nil or weapons[source][2] ~= weapon or weapons[source][3] ~= isPedDucked(source)) then -- Model never created
+			weapons[source][1] = createModel(source, weapon)
+			weapons[source][2] = weapon
+			weapons[source][3] = isPedDucked(source)
+		else
+			local object = weapons[source][1]
+			destroyElement(object)
+			weapons[source] = nil
 		end
-	else
-		if (objectmade) then
-			objectmade = false
-			triggerServerEvent("destroyWeaponBackModel", getLocalPlayer())
-		end
+	elseif (weapons[source][1] ~= nil) then
+		local object = weapons[source][1]
+		destroyElement(object)
+		weapons[source] = nil
 	end
 end
-addEventHandler("onClientRender", getRootElement(), weaponSwitched)
+addEventHandler("onClientPlayerWeaponSwitch", getRootElement(), weaponSwitch)
+
+function createModel(player, weapon)
+	local bx, by, bz = getPedBonePosition(player, 3)
+	local x, y, z = getElementPosition(player)
+	local r = getPedRotation(player)
+				
+	crouched = isPedDucked(player)
+	
+	local ox, oy, oz = bx-x-0.13, by-y-0.25, bz-z+0.25
+	
+	if (crouched) then
+		oz = -0.025
+	end
+	
+	local objectID = 355
+
+	if (weapon==31) then
+		objectID = 356
+	elseif (weapon==30) then
+		objectID = 355
+	end
+	
+	local currobject = getElementData(source, "weaponback.object")
+	if (isElement(currobject)) then
+		destroyElement(currobject)
+	end
+	
+	local object = createObject(objectID, x, y, z)
+	local attach = attachElements(object, player, ox, oy, oz, 0, 60, 0)
+	outputDebugString(tostring(attach))
+	return object
+end

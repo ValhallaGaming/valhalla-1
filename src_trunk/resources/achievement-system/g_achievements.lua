d50cf9e988789a094c5167c3bc4c64639c4fc5c1
Fenix1053 <Fenix1053@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-09-17 06:33:39 +0000

    Flood achievement
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1869 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/achievement-system/g_achievements.lua
+++ src_trunk/resources/achievement-system/g_achievements.lua
@@ -38,7 +38,8 @@ local g_achievements = {
 	{ "The Good Samaritan", "Donate $1000 or more to charity", 100 },
 	{ "Welcome to Los Santos", "Arrive in Los Santos.", 10 },
 	{ "Party Time", "Join the 2.1 Relaunch Party.", 100 },
-	{ "Where the Past Lies", "Which bones are those in the grave?", 25 }
+	{ "Where the Past Lies", "Which bones are those in the grave?", 25 },
+	{ "Sea World Los Santos", "Survived the 1st Flood of Los Snatos.", 100 }
 }
 
 function getAchievementInfo(id)

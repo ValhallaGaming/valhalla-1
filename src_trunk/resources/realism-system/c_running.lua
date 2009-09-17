1da4c5d8e258e0a4439cc75234340b15b24018d9
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-09-17 03:17:09 +0000

    increased running time
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1849 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_running.lua
+++ src_trunk/resources/realism-system/c_running.lua
@@ -4,7 +4,7 @@ addEventHandler( "onClientPreRender", getRootElement(),
 	function( slice )
 		if getControlState( "sprint" ) then
 			run = run + slice
-			if run >= 12000 then
+			if run >= 20000 then
 				exports.global:applyAnimation(getLocalPlayer(), "FAT", "idle_tired", 5000, true, false, true)
 			end
 		elseif run > 0 then

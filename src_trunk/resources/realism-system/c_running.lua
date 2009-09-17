8971e23b00d41781827ce884b0ea3325dff98ca9
mabako <mabako@64450a49-1f69-0410-a0a2-f5ebb52c4f5b>
2009-09-17 11:57:38 +0000

    0001425: Infinite Running Even With Tiredness
    git-svn-id: http://svn2.xp-dev.com/svn/Fenix1053-MTARP@1874 64450a49-1f69-0410-a0a2-f5ebb52c4f5b

--- src_trunk/resources/realism-system/c_running.lua
+++ src_trunk/resources/realism-system/c_running.lua
@@ -1,14 +1,22 @@
 local run = 0
+local down = 0
 
 addEventHandler( "onClientPreRender", getRootElement(), 
 	function( slice )
-		if getControlState( "sprint" ) then
+		if getControlState( "sprint" ) or down > 0 then
 			run = run + slice
 			if run >= 20000 then
 				exports.global:applyAnimation(getLocalPlayer(), "FAT", "idle_tired", 5000, true, false, true)
 			end
-		elseif run > 0 then
-			run = math.max( 0, run - math.ceil( slice / 3 ) )
+			if not getControlState( "sprint" ) then
+				down = math.max( 0, down - slice )
+			else
+				down = 500
+			end
+		else
+			if run > 0 then
+				run = math.max( 0, run - math.ceil( slice / 3 ) )
+			end
 		end
 	end
 )
